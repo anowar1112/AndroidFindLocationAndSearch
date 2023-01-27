f8b2ec3333bc0a068b5f7bec02641034d015924e
Anowar Hossain <anowar1112@gmail.com>
2023-01-28 00:43:33 +0600

    Location Find Project
    Signed-off-by: Anowar Hossain <anowar1112@gmail.com>

---
+++ app/src/main/java/com/first/mytestingfirstapps/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.first.mytestingfirstapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val SECOND_ACTIVITY_REQUEST_CODE = 10
+    private var text:EditText? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        text = findViewById<View>(R.id.eidText) as EditText
+        val textView = findViewById<View>(R.id.text) as TextView
+        textView.setOnClickListener {
+            val intent = Intent(this, MapsActivityCurrentPlace::class.java)
+            intent.putExtra("FirstActivityKey", text?.text.toString())
+
+            // startActivity(intent)
+            startActivityForResult(intent,SECOND_ACTIVITY_REQUEST_CODE)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            if(requestCode == SECOND_ACTIVITY_REQUEST_CODE){
+                val returnString = data!!.getStringExtra(Intent.EXTRA_TEXT)
+                text?.text = returnString?.toEditable()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/first/mytestingfirstapps/SecondActivity.kt
@@ -0,0 +1,54 @@
+package com.first.mytestingfirstapps
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val text = findViewById<View>(R.id.eidText2) as EditText
+        val backButtonForPreviousActivity = findViewById<View>(R.id.text2) as TextView
+        val goToOtherApps = findViewById<View>(R.id.text3) as TextView
+
+
+        // receive the value by getStringExtra() method and
+        // key must be same which by first activity
+        // receive the value by getStringExtra() method and
+        // key must be same which by first activity
+        val str = intent.getStringExtra("FirstActivityKey")
+        // display the string into textView
+        text.text = str?.toEditable()
+
+        goToOtherApps.setOnClickListener {
+            sendWhatsapp(text.text.toString())
+            //injectLaunchIntent()
+            //Toast.makeText(this, "There is no package available in android", Toast.LENGTH_LONG).show();
+        }
+
+        backButtonForPreviousActivity.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(Intent.EXTRA_TEXT, text.text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    private fun sendWhatsapp(message: String) {
+        val url = "https://api.whatsapp.com/send?phone=+8801739841696&text=$message"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        i.data = Uri.parse(url)
+        startActivity(i)
+        // Toast.makeText(this, "There is no package available in android", Toast.LENGTH_LONG).show()
+    }
+}
+
+
+fun String.toEditable(): Editable =  Editable.Factory.getInstance().newEditable(this)
